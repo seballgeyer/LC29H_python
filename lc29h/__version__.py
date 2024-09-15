0b40d7e6a6b1d4ee7ac338f851a70fd637ec8f91
Sebastien <sebastien.allgeyer@gmail.com>
2024-09-15 15:32:18 +1000

    adding the version to ignore

--- lc29h/__version__.py
+++ lc29h/__version__.py
@@ -12,5 +12,5 @@ __version__: str
 __version_tuple__: VERSION_TUPLE
 version_tuple: VERSION_TUPLE
 
-__version__ = version = '0.0.post2+dirty'
+__version__ = version = '0.0.post11+dirty'
 __version_tuple__ = version_tuple = (0, 0, 'dirty')
